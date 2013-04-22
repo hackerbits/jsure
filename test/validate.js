4042808dc13c050131e44376eaf2e1447d8768b8
Hackerbits <support@hackerbits.com>
2013-04-22 20:22:29 +0200

    Rename alwaysTrue and alwaysFalse to t and f

--- test/validate.js
+++ test/validate.js
@@ -1,30 +1,31 @@
 var assert = require("assert"),
+    L = require("lemonad"),
     j = require("../src/jsure");
 
-var alwaysTrue = function () { return true; };
+var t = L.always(true);
 
-var alwaysFalse = function () { return false; };
+var f = L.always(false);
 
 suite("validate", function () {
   test("false", function () {
-    assert.strictEqual(j.validate(alwaysFalse), false);
-    assert.strictEqual(j.validate([alwaysFalse]), false);
-    assert.strictEqual(j.validate([alwaysFalse, alwaysFalse]), false);
-    assert.strictEqual(j.validate([alwaysTrue, alwaysFalse]), false);
-    assert.strictEqual(j.validate([[alwaysFalse]]), false);
-    assert.strictEqual(j.validate([[[alwaysFalse]]]), false);
+    assert.strictEqual(j.validate(f), false);
+    assert.strictEqual(j.validate([f]), false);
+    assert.strictEqual(j.validate([f, f]), false);
+    assert.strictEqual(j.validate([t, f]), false);
+    assert.strictEqual(j.validate([[f]]), false);
+    assert.strictEqual(j.validate([[[f]]]), false);
   });
   test("true", function () {
-    assert.strictEqual(j.validate(alwaysTrue), true);
+    assert.strictEqual(j.validate(t), true);
     assert.strictEqual(j.validate([]), true);
-    assert.strictEqual(j.validate([alwaysTrue]), true);
-    assert.strictEqual(j.validate([alwaysTrue, alwaysTrue]), true);
-    assert.strictEqual(j.validate([[alwaysTrue]]), true);
-    assert.strictEqual(j.validate([[[alwaysTrue]]]), true);
-    assert.strictEqual(j.validate([[alwaysFalse], [alwaysTrue]]), true);
+    assert.strictEqual(j.validate([t]), true);
+    assert.strictEqual(j.validate([t, t]), true);
+    assert.strictEqual(j.validate([[t]]), true);
+    assert.strictEqual(j.validate([[[t]]]), true);
+    assert.strictEqual(j.validate([[f], [t]]), true);
   });
   test("logic", function () {
-    j.validate([alwaysFalse, function () { throw new Error("logic"); }]);
-    j.validate([[alwaysTrue], [function () { throw new Error("logic"); }]]);
+    j.validate([f, function () { throw new Error("logic"); }]);
+    j.validate([[t], [function () { throw new Error("logic"); }]]);
   });
 });
